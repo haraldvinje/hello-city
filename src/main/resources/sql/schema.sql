52ebe8882717c6c63f49562e49d73b078ee80763
Harald Vinje <haraldvinje@gmail.com>
2022-09-01 16:10:35 +0200

    Adding liquibase

--- src/main/resources/sql/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS cities(
-  id                     VARCHAR(60)  DEFAULT RANDOM_UUID() PRIMARY KEY,
-  name                   VARCHAR      NOT NULL,
-  description            VARCHAR      NOT NULL,
-  slug                   VARCHAR      NOT NULL,
-  added_at               DATETIME     NOT NULL
-);
